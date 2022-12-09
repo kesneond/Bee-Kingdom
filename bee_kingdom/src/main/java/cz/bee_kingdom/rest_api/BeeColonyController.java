4599638a565feab6e892f0c53772a5e5f778943c
kesneond <kesneond@fit.cvut.cz>
2022-12-09 20:37:58 +0100

    Adding openapi dependency

--- bee_kingdom/src/main/java/cz/bee_kingdom/rest_api/BeeColonyController.java
+++ bee_kingdom/src/main/java/cz/bee_kingdom/rest_api/BeeColonyController.java
@@ -1,11 +1,14 @@
 package cz.bee_kingdom.rest_api;
 
+import cz.bee_kingdom.business.EntityStateException;
 import cz.bee_kingdom.domain.BeeColony;
 import cz.bee_kingdom.rest_api.dto.bee_colony.BeeColonyDTO;
 import cz.bee_kingdom.rest_api.dto.bee_colony.BeeColonyToDto;
 import cz.bee_kingdom.rest_api.dto.bee_colony.BeeColonyToEntity;
 import cz.bee_kingdom.business.BeeColonyService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/colonies")
@@ -15,5 +18,12 @@ public class BeeColonyController extends AbstractCrudController<BeeColony, BeeCo
         super(beeColonyService, beeColonyToDto, beeColonyToEntity);
     }
 
-
+    @GetMapping("/{id}")
+    public BeeColonyDTO getById(@PathVariable Long id) {
+        Optional<BeeColony> tmp = super.service.readByID(id);
+        if(tmp.isEmpty()){
+            throw new EntityStateException("No Bee colony with name " + id);
+        }
+        return super.toDtoConverter.apply(tmp.get());
+    }
 }
