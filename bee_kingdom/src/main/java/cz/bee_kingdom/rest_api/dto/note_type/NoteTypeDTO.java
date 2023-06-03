3d865eadeac4bb8ef8f100924a653628f4d16b38
Petr Franta <petr.franta@gmail.com>
2023-06-03 21:31:54 +0200

    Odstranění nadbytečného kódu

--- bee_kingdom/src/main/java/cz/bee_kingdom/rest_api/dto/note_type/NoteTypeDTO.java
+++ bee_kingdom/src/main/java/cz/bee_kingdom/rest_api/dto/note_type/NoteTypeDTO.java
@@ -5,12 +5,4 @@ import lombok.Data;
 @Data
 public class NoteTypeDTO {
     public String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
